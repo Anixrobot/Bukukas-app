3823dd4ccf32f2fb825c02e7cecb5a140371fdfb
unknown <rafiearmansyah@gmail.com>
2026-07-21 17:38:28 +0700

    nambahin php lainnya

---
+++ resources/views/dashboard-kelas.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('konten')
+<div class="card shadow-sm border-0">
+    <div class="card-body">
+        <h3 class="card-title text-primary">Workspace Kas Kelas</h3>
+        <p class="text-muted">Ini adalah area khusus untuk mengelola uang kelas dan data siswa.</p>
+        <hr>
+        <!-- Nanti form input dan tabel kita taruh sini -->
+        <p><i>(Tabel dan form menyusul...)</i></p>
+    </div>
+</div>
+@endsection
---
+++ resources/views/dashboard-pribadi.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('konten')
+<div class="card shadow-sm border-0">
+    <div class="card-body">
+        <h3 class="card-title text-success">Workspace Keuangan Pribadi</h3>
+        <p class="text-muted">Rahasia aman. Catat semua jajan, bensin, dan pemasukan lu di sini.</p>
+        <hr>
+        <!-- Nanti form input dan tabel kita taruh sini -->
+        <p><i>(Tabel dan form menyusul...)</i></p>
+    </div>
+</div>
+@endsection
